1eee3644ce5f3bb1747f0720a3a6028269815207
Louiz Kim-Chan <kimblader151@yahoo.com.sg>
2020-08-11 00:59:02 +0800

    Save leads to database.

--- controllers/landing.js
+++ controllers/landing.js
@@ -7,7 +7,6 @@ exports.get_landing = function(req, res, next) {
 
 exports.submit_lead = function(req, res, next) {
   // Prints to our server side console the input from the field which we named "lead_email"
-  console.log("lead email", req.body.lead_email);
   return models.Lead.create({
     email: req.body.lead_email
   }).then(lead => {
