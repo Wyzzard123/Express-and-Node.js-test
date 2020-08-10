a59236fcad783f34a5e12edbd136e6b3d09d2033
Louiz Kim-Chan <kimblader151@yahoo.com.sg>
2020-08-10 23:58:21 +0800

    Added email form post method

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,5 +1,12 @@
+
 exports.get_landing = function(req, res, next) {
   //  First argument = template (see views/landing.pug)
   //  Second argument = JSON with title which we reference in the landing.pug file
   res.render('landing', { title: 'Express' });
+}
+
+exports.submit_lead = function(req, res, next) {
+  // Prints to our server side console the input from the field which we named "lead_email"
+  console.log("lead email", req.body.lead_email);
+  res.redirect('/');
 }
--- routes/landing.js
+++ routes/landing.js
@@ -6,5 +6,6 @@ let landing = require('../controllers/landing')
 /* GET home page. */
 //Assigned handler function to landing.js
 router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead);
 
 module.exports = router;
