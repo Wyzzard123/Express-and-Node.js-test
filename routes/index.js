0c2f050fe2be0360f0e70bed99d1a3606101837c
Louiz Kim-Chan <kimblader151@yahoo.com.sg>
2020-08-11 14:49:06 +0800

    Renamed landing.js in routes to index.js, implemented /leads route to show leads with same landing.pug template using if statements

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var router = express.Router();
 let landing = require('../controllers/landing')
 
 /* GET home page. */
-//Assigned handler function to landing.js
+//Assigned handler function to index.js
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
+router.get('/leads', landing.show_leads);
 
 module.exports = router;
